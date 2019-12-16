0bdc1dc6cfd7a033b3b3b79b699d9e5e8886cf7c
Anne Terechowa <annevonwolffen@gmail.com>
2019-12-16 16:06:53 +0300

    fix notification content text

--- app/src/main/java/com/annevonwolffen/androidschool/taskmanager/ui/alarm/AlarmReceiver.java
+++ app/src/main/java/com/annevonwolffen/androidschool/taskmanager/ui/alarm/AlarmReceiver.java
@@ -88,7 +88,7 @@ public class AlarmReceiver extends BroadcastReceiver {
                 CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_event_note_black_24dp)
                 .setContentTitle(label)
-                .setContentText(R.string.notification_content_text + dateTime)
+                .setContentText(context.getString(R.string.notification_content_text) + " " + dateTime)
                 .setOnlyAlertOnce(true)
                 .setSound(alarmSound)
                 .setAutoCancel(true)
